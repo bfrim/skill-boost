2306dfbb65c93fcc25914cec5c1cea52c9eee397
LiamWalenius <liamwalenius@gmail.com>
2024-09-28 13:14:36 -0400

    add calendar-entry.js

---
+++ calendar-entry.js
@@ -0,0 +1,8 @@
+class CalendarEntry {
+    constructor (startDate, endDate, activity, notes) {
+        this.startDate = startDate
+        this.endDate = endDate
+        this.activity = activity
+        this.notes = notes
+    }
+}
